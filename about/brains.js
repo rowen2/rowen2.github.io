833207e6c4d5a11697b2675ce2c90ba8457dc509
Jeremy Whitman <jwhitman@murraystate.edu>
2016-11-25 15:03:54 -0600

    Update brains.js

--- about/brains.js
+++ about/brains.js
@@ -1,7 +1,7 @@
 var dataBaseDevs = new Firebase("https://mum-database.firebaseio.com");
 
 function onDev1(){
-  var dev1 = firebase.dataBaseDevs().ref('Developers/Dev1');
+  var dev1 = firebase.dataBaseDevs().ref('Developers/Dev1/');
   dev1.on('value', function(snapshot) {
     document.getElementById("p0").innerHTML = snapshot.val()); 
   });
