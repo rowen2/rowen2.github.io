fea4b25f58f2fe62aee2f76fad925d3e65f9f27d
Jeremy Whitman <jwhitman@murraystate.edu>
2016-11-25 14:44:04 -0600

    Update brains.js

--- about/brains.js
+++ about/brains.js
@@ -1,4 +1,6 @@
-var dev1 = firebase.database().ref("Developers/Dev1");
-dev1.on('value', function(snapshot) {
-  document.getElementById("p0").innerHTML = "snapshot.val())"; 
-});
+function onDev1(){
+  var dev1 = firebase.database().ref("Developers/Dev1");
+  dev1.on('value', function(snapshot) {
+    document.getElementById("p0").innerHTML = "snapshot.val())"; 
+  });
+}
