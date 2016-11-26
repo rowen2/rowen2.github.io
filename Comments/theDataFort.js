fed46088dca627c58d1ddbb952cd3124a9470250
Jeremy Whitman <jwhitman@murraystate.edu>
2016-11-25 19:46:38 -0600

    Update theDataFort.js

--- Comments/theDataFort.js
+++ Comments/theDataFort.js
@@ -4,17 +4,14 @@ function populateComments(){
     document.getElementById("previousC").innerHTML = snapshot.val();
   });
 }//not yet done
-function writeComments(commentID, comment) {
-  var newKey = firebase.database().ref().child('Comments').push().key;
-
-  var updates = {};
-  updates['/Comments/' + newKey] = comment;
-  return firebase.database().ref().update(updates);
+function writeComments(comment) {
+  firebase.database().ref('Comments/').push({
+    Comment: comment
+  });
 
 }
 function getVariables(){
-      var ID = document.getElementById("cID").value;
       var CM = document.getElementById("com").value;
-      writeComments(Com2, data);
+      writeComments(data);
 }
 
