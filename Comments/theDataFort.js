8e3eff71a605f4fb0ba1beb7e73b43658bff8949
Jeremy Whitman <jwhitman@murraystate.edu>
2016-11-26 17:33:45 -0600

    Update theDataFort.js

--- Comments/theDataFort.js
+++ Comments/theDataFort.js
@@ -6,7 +6,7 @@ function populateComments(){
 }
 function writeComments(name, comment) {  
     var newComment = firebase.database().ref('/Comments');
-    newComment.push({ name : comment});
+    newComment.push({ 'Name': name , 'Comment' : comment});
   }
 
 function getVariables(){
