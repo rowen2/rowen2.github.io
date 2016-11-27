f6c822df2a261c242f7c0a0279cff7e67710fb66
Jeremy Whitman <jwhitman@murraystate.edu>
2016-11-27 15:09:24 -0600

    Update theDataFort.js

--- Comments/theDataFort.js
+++ Comments/theDataFort.js
@@ -11,7 +11,7 @@ function writeComments(name, comment) {
   var newCommentRef = newComment.push();
     newCommentRef.set({ 'Comment' : comment, 'Name': name });
     // var path = newCommentRef.key().toString();
-  document.getElementById("previousC").innerHTML ="hello";
+  document.getElementById("previousC").innerHTML =newCommentRef.toString();
   populateComments("monkeys");
   }
 
