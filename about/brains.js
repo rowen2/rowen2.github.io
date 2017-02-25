42f09743383430f1a5a6263c02b474c7aca1be4c
Jeremy Whitman <jwhitman@murraystate.edu>
2017-02-25 14:03:17 -0600

    Delete brains.js

--- about/brains.js
+++
@@ -1,8 +0,0 @@
-
-function onDev1(){
-  var iHateThis = firebase.database().ref('/Developers/Dev1');
-  iHateThis.on('value', function(snapshot) {
-    document.getElementById("p0").innerHTML = snapshot.val();
-  });
-}
-  
